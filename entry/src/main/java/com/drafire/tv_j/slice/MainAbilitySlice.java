da45a3186e39f6540ba29a55d0b1b17e776d25b4
drafire <hyljljlj@163.com>
2020-09-19 11:09:35 +0800

    1、hello word

--- entry/src/main/java/com/drafire/tv_j/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/drafire/tv_j/slice/MainAbilitySlice.java
@@ -1,37 +1,15 @@
 package com.drafire.tv_j.slice;
 
+import com.drafire.tv_j.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
 
-import ohos.agp.components.DirectionalLayout;
-import ohos.agp.components.DirectionalLayout.LayoutConfig;
-import ohos.agp.components.Text;
-import ohos.agp.colors.RgbColor;
-import ohos.agp.components.element.ShapeElement;
-import ohos.agp.utils.Color;
-import ohos.agp.utils.TextAlignment;
-
 public class MainAbilitySlice extends AbilitySlice {
-
-    private DirectionalLayout myLayout = new DirectionalLayout(this);
 
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
-        LayoutConfig config = new LayoutConfig(LayoutConfig.MATCH_PARENT, LayoutConfig.MATCH_PARENT);
-        myLayout.setLayoutConfig(config);
-        ShapeElement element = new ShapeElement();
-        element.setRgbColor(new RgbColor(255, 255, 255));
-        myLayout.setBackground(element);
-
-        Text text = new Text(this);
-        text.setLayoutConfig(config);
-        text.setText("Hello World");
-        text.setTextColor(new Color(0xFF000000));
-        text.setTextSize(50);
-        text.setTextAlignment(TextAlignment.CENTER);
-        myLayout.addComponent(text);
-        super.setUIContent(myLayout);
+        super.setUIContent(ResourceTable.Layout_main_layout);
     }
 
     @Override
